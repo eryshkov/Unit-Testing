7ca31ff34a6019ce291ae333ca6ea76d4d8b54bd
Evgeniy Ryshkov <eryshkov@gmail.com>
2019-01-16 10:35:43 +0300

    Initial Commit

---
+++ Unit Testing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Unit Testing
+//
+//  Created by Evgeniy Ryshkov on 16/01/2019.
+//  Copyright © 2019 Evgeniy Ryshkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Unit TestingTests/Unit_TestingTests.swift
@@ -0,0 +1,34 @@
+//
+//  Unit_TestingTests.swift
+//  Unit TestingTests
+//
+//  Created by Evgeniy Ryshkov on 16/01/2019.
+//  Copyright © 2019 Evgeniy Ryshkov. All rights reserved.
+//
+
+import XCTest
+@testable import Unit_Testing
+
+class Unit_TestingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
